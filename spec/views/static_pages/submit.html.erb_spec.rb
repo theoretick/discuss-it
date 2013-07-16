beeb7ca6b51e2f69ba78d11657db4a84df147001
Johann <jhannesson@gmail.com>
2013-07-16 15:54:29 -0700

    changed capybara tests on submit

--- spec/views/static_pages/submit.html.erb_spec.rb
+++ spec/views/static_pages/submit.html.erb_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe "static_pages/submit.html.erb" do
 
   it "should have an anchor tag 'a'" do
-    visit '/static_pages/submit'
+    visit '/'
+    fill_in 'query', :with => 'www.restorethefourth.net/'
+    click_button 'Search'
     page.has_content?('TOP RESULTS')
   end
 
@@ -14,28 +16,34 @@ describe "static_pages/submit.html.erb" do
     expect(page).to have_selector("a")
   end
 
-  it "should load from search button" do
+  it "should not load from index search button" do
     visit '/'
     click_button 'Search'
-    current_path.should == static_pages_submit_path
+    current_path.should == root_path
   end
 
   it "should link to about page" do
     visit '/'
+    fill_in 'query', :with => 'www.restorethefourth.net/'
+    click_button 'Search'
     click_link 'About'
     current_path.should == static_pages_about_path
   end
 
   it "Should offer a try again link with invalid search" do
     visit '/'
+    fill_in 'query', :with => 'http://sl.wikipedia.org/wiki/Muslimanski_koledar'
     click_button 'Search'
     click_link 'Try again?'
     current_path.should == root_path
   end
 
   it "Should have a link back to home page" do
-    visit '/static_pages/submit'
-    page.has_link?('Discuss it')
+    visit '/'
+    fill_in 'query', :with => 'www.restorethefourth.net/'
+    click_button 'Search'
+    click_link('Discuss it')
+    current_path.should == root_path
   end
 
 end
