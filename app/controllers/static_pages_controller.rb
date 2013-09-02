2aa9a0b0aab4de083ab9b82a6030e436184b484c
theoretick <me@lucascharles.me>
2013-09-02 15:26:28 -0700

    added score+comment_count to submit page, better json render with hits per result block

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,25 +10,34 @@ class StaticPagesController < ApplicationController
     begin
 
       @query_url = params[:url]
-
       # checks for specific version number, 2 skips slashdot
       @api_version = 2 if params[:version] == '2'
 
-      @discuss_it = DiscussIt::DiscussItApi.new(@query_url, @api_version)
+      discuss_it = DiscussIt::DiscussItApi.new(@query_url, @api_version)
 
-      @all_results = @discuss_it.find_all
-      @top_results = @discuss_it.find_top
+      # result_type = params[:result_type]
 
-      @hits = set_total_results
+      @top_results = discuss_it.find_top
+      top_results = {
+           hits: set_top_hits,
+        results: @top_results
+      }
+
+      @all_results = discuss_it.find_all.all
+      all_results = {
+           hits: set_all_hits,
+        results: @all_results
+      }
 
       # provides nicely-formatted JSON return with hit count as first val
       respond_to do |format|
         format.html
         format.json {
-          render :json => {
-            :hits => @hits,
-            :top_results => @top_results,
-            :all_results => @all_results
+          render json: {
+            # TODO: this total is nonconditional, should change w/ params
+             total_hits: set_total_hits,
+            top_results: top_results,
+            all_results: all_results
           }
         }
       end
@@ -46,8 +55,18 @@ class StaticPagesController < ApplicationController
   private
 
   # returns the total number of hits for top + all results
-  def set_total_results
-    return @all_results.all.length + @top_results.length
+  def set_total_hits
+    return set_top_hits + set_all_hits
+  end
+
+  # returns the total number of hits for top results
+  def set_top_hits
+    return @top_results.length || 0
+  end
+
+  # returns the total number of hits for all results
+  def set_all_hits
+    return @all_results.length || 0
   end
 
 end
