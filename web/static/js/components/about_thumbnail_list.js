aae46800566365df768c94e1b797eeebcbcab6ad
Lucas Charles <me@lucascharles.me>
2016-11-18 16:36:11 -0800

    Remove spurious params on avatar URLs

--- web/static/js/components/about_thumbnail_list.js
+++ web/static/js/components/about_thumbnail_list.js
@@ -5,17 +5,17 @@ import AboutThumbnailItem from './about_thumbnail_item'
 const ABOUT_CONTRIBUTORS = [
   {
     username: 'theoretick',
-    imgUrl: 'https://secure.gravatar.com/avatar/68a526b3e51ee3ed4c1d10e8b7fbd3c7?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+    imgUrl: 'https://secure.gravatar.com/avatar/68a526b3e51ee3ed4c1d10e8b7fbd3c7?s=420',
     githubUrl: 'https://github.com/theoretick'
   },
   {
     username: 'CodingAntecedent',
-    imgUrl: 'https://secure.gravatar.com/avatar/81060418d41061aade8c8f7d74b584c4?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+    imgUrl: 'https://secure.gravatar.com/avatar/81060418d41061aade8c8f7d74b584c4?s=420',
     githubUrl: 'https://github.com/codingantecedent'
   },
   {
     username: 'ericalthatcher',
-    imgUrl: 'https://secure.gravatar.com/avatar/7aa1140a4a9a313576971b9e74d9f90d?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+    imgUrl: 'https://secure.gravatar.com/avatar/7aa1140a4a9a313576971b9e74d9f90d?s=420',
     githubUrl: 'https://github.com/ericalthatcher'
   }
 ]
