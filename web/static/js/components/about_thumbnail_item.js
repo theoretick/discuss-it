c4b9b6c683b98b9a91a5c099ccd6f9f8fa73eb56
Lucas Charles <me@lucascharles.me>
2017-01-02 10:17:07 -0800

    Refactor: simplify about thumbnails

--- web/static/js/components/about_thumbnail_item.js
+++ web/static/js/components/about_thumbnail_item.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
 
-const AboutThumbnailItem = ({contributor}) => {
-  var {username, imgUrl, githubUrl} = contributor
+const AboutThumbnailItem = ({ contributor }) => {
+  var { username, imgUrl, githubUrl } = contributor
 
   return (
     <li className="col-md-4">
       <div className="thumbnail">
-        <img src={imgUrl} />
-        <div className="caption text-center">
-          <h4>{username}</h4>
-          <a className="btn btn-primary btn-block" href={githubUrl}>
-            Github Profile
-          </a>
-        </div>
+        <a href={githubUrl}>
+          <img src={imgUrl} />
+          <div className="caption text-center">
+            <h4>{username}</h4>
+          </div>
+        </a>
       </div>
     </li>
   )
